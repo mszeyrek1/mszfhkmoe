5cb36c6cec461639436a37aa0294f5ad825d34f6
Mszeyrek <muhammed210606@gmail.com>
2024-06-23 16:45:42 +0200

    Initial Commit

---
+++ recordinput.cpp
@@ -0,0 +1,17 @@
+#include <chrono>
+#include <iostream>
+#include <string>
+
+
+int input_detecter(int input) {
+    int counter = 0;
+    if (input = isPressed()) {
+        counter++;
+        return counter;
+    }
+}
+
+
+int main() {
+    return 0;
+}
